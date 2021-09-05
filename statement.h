c5e6a441a1cfd1ced7b53406586506e6f34a4954
Qianxuan <2392009257@qq.com>
2021-09-06 00:50:24 +0800

    add_EndT & type _ArcLine

--- statement.h
+++ statement.h
@@ -15,59 +15,73 @@ extern long long ID;
 
 const double SMALL_ = 5.0 , MIDDLE_ = 7.0 , LARGE_ = 10.0 ;           // 弹幕大小
 const double LOW_ = 0.06 , NORMAL_ = 0.09 , FAST_ = 0.12 ;              // 弹幕速度
-enum Kind { Line , Circle };                                             // 弹幕类型
+enum Kind { Line , Circle , ArcLine };                                             // 弹幕类型
 
 const magi::Color c1(240, 128, 128),c2(244, 151, 142),c3(248, 173, 157),c4(251, 196, 171),c5(255, 218, 185);
 
 
 // 弹幕样式
 struct Bullet_Style {
-    Bullet_Style () { speed = LOW_ ; point = magi::Point ( 0 , magi::Color() , SMALL_ ) ; angle = 0.0 ; StartT = 0 ; };
-    Bullet_Style ( long long id , magi::Color c , double r , magi::Vec2 center , double angle , double speed , long long StartT ) : point (magi::Point(id,c,r)) ,angle (angle) , speed (speed) , StartT (StartT) , center(center) {}
+    Bullet_Style () { speed = LOW_ ; point = magi::Point ( 0 , magi::Color() , SMALL_ ) ; angle = 0.0 ; StartT = 0 ; EndT = 0 ; };
+    Bullet_Style ( long long id , magi::Color c , double r , magi::Vec2 center , double angle , double speed , long long StartT , long long EndT ) : point (magi::Point(id,c,r)) ,angle (angle) , speed (speed) , StartT (StartT) , EndT (EndT) , center(center) {}
     magi::Point point;
     magi::Vec2 center;
     double angle , speed ;
-    long long StartT;
+    long long StartT , EndT ;
     virtual magi::Vec2 Pos() {}
 };
 
 // 弹幕运动方式实现
 // 直线
 struct Bullet_Line : public Bullet_Style {
-    Bullet_Line () {}
-    Bullet_Line ( long long id , magi::Color c , double r , magi::Vec2 center , double angle , double speed , long long StartT ) : Bullet_Style (id,c,r,center,angle,speed,StartT) {}
+    Bullet_Line ( long long id , magi::Color c , double r , magi::Vec2 center , double angle , double speed , long long StartT , long long EndT ) : Bullet_Style (id,c,r,center,angle,speed,StartT,EndT) {}
     magi::Vec2 Pos () {
         long long RelaT = magi::Timer::get() - StartT ;
         this -> point.pos.x = cos(angle)*speed*RelaT + center.x ;
         this -> point.pos.y = sin(angle)*speed*RelaT + center.y ;
-
         return this -> point.pos;
     }
 };
 
 // 曲线
 struct Bullet_Circal : public Bullet_Style {
-    Bullet_Circal () {}
-    Bullet_Circal ( long long id , magi::Color c , double r , magi::Vec2 center , double angle , double speed , long long StartT ) : Bullet_Style (id,c,r,center,angle,speed,StartT) {}
+    Bullet_Circal ( long long id , magi::Color c , double r , magi::Vec2 center , double angle , double speed , long long StartT , long long EndT ) : Bullet_Style (id,c,r,center,angle,speed,StartT,EndT) {}
     magi::Vec2 Pos () {
         long long RelaT = magi::Timer::get() - StartT ;
         point.pos.x = cos(angle)*speed*RelaT*RelaT ;                                                                                       // !!!待定
         point.pos.y = sin(angle)*speed*RelaT*RelaT ;
 
-        return point.pos;
+        return this -> point.pos;
     }
 };
 
-shared_ptr<Bullet_Style> Creat ( long long StartT , magi::Color c , double r , magi::Vec2 center , double angle , double speed , Kind kind ) {
+// 反向直线
+struct Bullet_ArcLine : public Bullet_Style {
+    Bullet_ArcLine ( long long id , magi::Color c , double r , magi::Vec2 center , double angle , double speed , long long StartT , long long EndT ) : Bullet_Style (id,c,r,center,angle,speed,StartT,EndT) {}
+    magi::Vec2 Pos () {
+        long long RelaT = EndT - (magi::Timer::get() - StartT) ;
+        this -> point.pos.x = cos(angle)*speed*RelaT + center.x ;
+        this -> point.pos.y = sin(angle)*speed*RelaT + center.y ;
+        return this -> point.pos;
+    }
+};
+
+
+shared_ptr<Bullet_Style> Creat ( long long StartT , long long EndT , magi::Color c , double r , magi::Vec2 center , double angle , double speed , Kind kind ) {
     shared_ptr<Bullet_Style> Re;
     switch (kind) {
             case Line:
-                Re = make_shared<Bullet_Line> ( Bullet_Line(ID,c,r,center,angle,speed,StartT) );
+                Re = make_shared<Bullet_Line> ( Bullet_Line(ID,c,r,center,angle,speed,StartT,EndT) );
                 ID++;
                 break;
             case Circle:
-                Re = make_shared<Bullet_Circal> ( Bullet_Circal(ID,c,r,center,angle,speed,StartT) );
+                Re = make_shared<Bullet_Circal> ( Bullet_Circal(ID,c,r,center,angle,speed,StartT,EndT) );
                 ID++;
+                break;
+            case ArcLine:
+                Re = make_shared<Bullet_ArcLine> ( Bullet_ArcLine(ID,c,r,center,angle,speed,StartT,EndT) );
+                ID++;
+                break;
                 break;
         }
     return Re;
@@ -78,7 +92,7 @@ struct Bullets_Info {
     Bullets_Info ( long long StartT , int n , magi::Color c , magi::Vec2 center , magi::Vec2 range , double r , double speed , Kind kind ) : n(n) , StartT(StartT) , range(range) {
         this -> EndT = StartT + 10000;                                                  // EndT 计算
             for ( int i = 0 ; i < n ; i++ ) {
-                this -> bullets.push_back( Creat ( StartT , c , r , center , (range.x + ((range.y-range.x)/n * i)) , speed , kind ) );
+                this -> bullets.push_back( Creat ( StartT , EndT , c , r , center , (range.x + ((range.y-range.x)/n * i)) , speed , kind ) );
         }
     }
     int n;
