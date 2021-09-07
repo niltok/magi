97a0874600a5e3f4e31d60b68e56457868351c69
玩火 <niltok@163.com>
2021-09-07 11:33:26 +0800

    add rainbow & move orenge

--- colors.h
+++ colors.h
@@ -6,11 +6,28 @@
 namespace colors {
 using namespace magi;
 using namespace std;
-vector<Color> rainbow {
+vector<Color> greenToRedLight {
     {168, 230, 207},
     {220, 237, 193},
     {255, 211, 182},
     {255, 170, 165},
     {255, 139, 148}
+};
+vector<Color> rainbowLight {
+    {255, 173, 173},
+    {255, 214, 165},
+    {253, 255, 182},
+    {202, 255, 191},
+    {155, 246, 255},
+    {160, 196, 255},
+    {189, 178, 255},
+    {255, 198, 255}
+};
+vector<Color> orengeToWhite {
+    {240, 128, 128},
+    {244, 151, 142},
+    {248, 173, 157},
+    {251, 196, 171},
+    {255, 218, 185}
 };
 }
