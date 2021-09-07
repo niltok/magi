704d53e33c74edaad63f6e908737371455ef6d38
玩火 <niltok@163.com>
2021-09-07 11:19:57 +0800

    add colors.h

---
+++ colors.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "interface.h"
+#include <vector>
+
+namespace colors {
+using namespace magi;
+using namespace std;
+vector<Color> rainbow {
+    {168, 230, 207},
+    {220, 237, 193},
+    {255, 211, 182},
+    {255, 170, 165},
+    {255, 139, 148}
+};
+}
