74df6b4e4e9ae4be059a9221f95824a5745bffd3
玩火 <niltok@163.com>
2021-09-23 16:27:03 +0800

    increase fps of zero len tail

--- imagedrawer.h
+++ imagedrawer.h
@@ -51,14 +51,17 @@ class ImageDrawer : public QThread {
         s = bullets->size();
         for (size_t i = 0; i < s; i++) {
             Point p = (*bullets)[i];
-            // auto pos = p.pos * scale + center;
             painter.setBrush(VColor(p.c));
-            if (!stage->visible(p)) continue;
-            p.tail.add(p.pos);
-            p.tail(painter);
-            // if (!pos.inRect(Vec2(-p.r * scale), widget + Vec2(p.r * scale))) continue;
-            // int r = p.r * scale;
-            // painter.drawEllipse(VPoint(pos), r, r);
+            if (p.tail.len) {
+                if (!stage->visible(p)) continue;
+                p.tail.add(p.pos);
+                p.tail(painter);
+            } else {
+                 auto pos = p.pos * scale + center;
+                 if (!pos.inRect(Vec2(-p.r * scale), widget + Vec2(p.r * scale)) || !stage->visible(p)) continue;
+                 int r = p.r * scale;
+                 painter.drawEllipse(VPoint(pos), r, r);
+            }
             // std::cout << p.id << std::endl;
         }
         painter.restore();
