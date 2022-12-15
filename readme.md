80548000336551b1a6a70ebce2b3110455092c5e
玩火 <niltok@163.com>
2022-12-16 02:47:39 +0800

    add examples

---
+++ readme.md
@@ -0,0 +1,14 @@
+# Usage
+
+Dependencies:
+- CMake
+- Qt 5.9.7
+- LAV Filter
+
+# Example
+
+![example1](./resource/image/example1.png)
+
+![example2](./resource/image/example2.png)
+
+![example3](./resource/image/example3.png)
--- stage.cpp
+++ stage.cpp
@@ -635,7 +635,7 @@ magi::Character ch_empty {"", magi::Vec2(), 1, magi::Color("00000000"), magi::Ve
 
 vector<magi::Stage> magi::Stage::stage {
     magi::Stage{"Death and Rebirth", getBullet_Re{0}, ch_adam, "OdeToJoy1.mp3",140000, "EVA1.jpg", magiUI::EVAEffects},
-    magi::Stage{"ReverseLine Bullets Test", getBullet_Re{1}, ch, "440.mp3",EndTime, ""},
+    // magi::Stage{"ReverseLine Bullets Test", getBullet_Re{1}, ch, "440.mp3",EndTime, ""},
     magi::Stage{"Magica Quartet", getBullet_Re{2}, ch, "Sis puella magica!.mp3", 167000, "madoka.png", magiUI::MagicaEffects},
     magi::Stage{"缩圈",getBullet_Re{3},ch_empty,"青花瓷.mp3", 229333, "", magiUI::ChinaEffects},
     // magi::Stage{"烟花",getBullet_Re{4},ch,"敬具.mp3",EndTime, ""}
